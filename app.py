8140970d7c181f1dd07fbf691a876573672f9754
aloui.wissem <alouiwiss@gmail.com>
2023-04-25 14:54:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker premier  test de deploiment version 19 </h2>'
+   return '''
+        <h1>Hello from Flask & Docker premier test de deploiment version 19</h2>
+        <img src="/static/image.jpg" alt="My image">
+    '''
 if __name__ == "__main__":
     app.run(debug=True)
