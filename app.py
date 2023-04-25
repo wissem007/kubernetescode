6124cff9c3e1749f4efe70cbab60d9fccf37d27e
aloui.wissem <alouiwiss@gmail.com>
2023-04-25 14:45:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker premier  test de deploiment </h2>'
+    return '<h1>Hello from Flask & Docker premier  test de deploiment version 19 </h2>'
 if __name__ == "__main__":
     app.run(debug=True)
